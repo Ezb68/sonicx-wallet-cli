c85c48ba2718c7cd5e7156d8857b41b46ba169d3
SonicX Blockchain <support@payus.io>
2019-04-12 09:27:47 +0700

    modify address prefix

--- src/main/java/org/tron/core/config/Parameter.java
+++ src/main/java/org/tron/core/config/Parameter.java
@@ -3,7 +3,7 @@ package org.tron.core.config;
 public interface Parameter {
 
   interface CommonConstant {
-    byte ADD_PRE_FIX_BYTE_MAINNET = (byte) 0x41;   //41 + address
+    byte ADD_PRE_FIX_BYTE_MAINNET = (byte) 0x40;   //40 + address
     byte ADD_PRE_FIX_BYTE_TESTNET = (byte) 0xa0;   //a0 + address
     int ADDRESS_SIZE = 21;
   }
